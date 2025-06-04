1ba62ee87e660dcbc95e14861f01d278749ff155
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-04 13:53:36 +0200

    Add haldler to server.js which allow to show html pages in case in case choose wrong page"

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ app.use(credentials);
 //Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("text").send("404 Not Found");
+  }
+});
+
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
