141f6651deb3617182186becb68341dee58c3e8c
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-04 18:18:53 +0200

    Do function handleLogout in logoutController.js

---
+++ controllers/logoutController.js
@@ -0,0 +1,37 @@
+import { db } from "../db.js";
+
+const handleLogout = async (req, res) => {
+  //on clinet, alos delate the accessToken
+  try {
+    const refreshToken = req.cookies.refresh_token;
+    if (refreshToken === null) return res.status(204);
+    try {
+      const foundUser = await db.query("SELECT * FROM users WHERE token =$1", [
+        refreshToken,
+      ]);
+      if (!foundUser) {
+        res.clearCookie("refresh_token");
+        return res.status(204);
+      }
+      // delete refreshToken in db
+      console.log(foundUser.rows[0].id);
+
+      try {
+        await db.query(
+          "UPDATE users SET token = $1 WHERE id = $2 RETURNING *",
+          ["", foundUser.rows[0].id]
+        );
+        res.clearCookie("refresh_token");
+        res.status(200).json({ message: "refresh token  del" });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+export { handleLogout };
