910c977d2b137b156e73c9655d145a7a2c67a514
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-04 13:05:45 +0200

    Add errorHandler to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { logger } from "./middleware/logEvents.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ const __dirname = path.dirname(__filename);
 const PORT = 3700;
 app.use(logger);
 
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
