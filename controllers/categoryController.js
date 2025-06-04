648da25b4b3a38d2dd248eb50d52add1b5e42672
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-04 18:49:44 +0200

    Add  categoryController.js function to add new category and import it in categoryRoute.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -27,4 +27,34 @@ const getAllCategriesUser = async (req, res) => {
   }
 };
 
-export { getAllCategories, getAllCategriesUser };
+const addCategryChoosenUser = async (req, res) => {
+  const { user_id, category } = req.body;
+
+  try {
+    const duplicate = await db.query(
+      "SELECT * FROM category WHERE user_id = $1 AND  category = $2",
+      [user_id, category]
+    );
+
+    if (duplicate.rowCount !== 0)
+      return res.status(400).json({ message: "Category already exist" });
+    if (category === "")
+      return res.status(400).json({ message: "Category name can't be empty" });
+    try {
+      const newCategory = await db.query(
+        "INSERT INTO category(user_id, category) VALUES($1,$2) RETURNING *",
+        [user_id, category]
+      );
+
+      res.status(201).json({
+        success: `New category ${newCategory.rows[0].category} created`,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { getAllCategories, getAllCategriesUser, addCategryChoosenUser };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllCategories,
   getAllCategriesUser,
+  addCategryChoosenUser,
 } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.get("/", getAllCategories);
 router.get("/user/:id", getAllCategriesUser);
+router.post("/add", addCategryChoosenUser);
 
 export default router;
