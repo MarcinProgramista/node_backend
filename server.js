2f8be3bc50ef5cd79012b77d28f84f741d5c8039
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-04 13:47:40 +0200

    Add corsOptions to server.js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 import express from "express";
-import { logger } from "./middleware/logEvents.js";
 import path from "path";
+import cors from "cors";
+import { logger } from "./middleware/logEvents.js";
 import { fileURLToPath } from "url";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { credentials } from "./middleware/credentials.js";
-
+import { corsOptions } from "./config/corsOptions.js";
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = 3700;
+
+//custom middleware logger
 app.use(logger);
 app.use(credentials);
+//Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 app.use(errorHandler);
 app.listen(PORT, () => {
