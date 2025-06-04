f7554e3844c73395e550374b21b00bb390200c8d
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-06-04 13:01:00 +0200

    Add logger to server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { logger } from "./middleware/logEvents.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = 3700;
+app.use(logger);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
