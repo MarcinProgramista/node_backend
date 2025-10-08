01bbe5d318e0d2a57ddde52483f77a61d7097509
MarcinProgramista <48890551+MarcinProgramista@users.noreply.github.com>
2025-10-08 18:44:50 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,8 +3,10 @@ const allowedOrigins = [
   "http://localhost:3700",
   "http://localhost:5173",
   "http://localhost:5174",
+   "http://127.0.0.1:5000",
   "http://localhost:3700/api/auth/login",
 ];
 
 export { allowedOrigins };
 //
+
